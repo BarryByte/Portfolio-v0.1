6ac40781218bb838777c6466e07e62569378fd63
BarryByte <abhayraj.12667@gmail.com>
2024-06-12 17:07:26 +0530

    project-page adjusted

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,21 +1,22 @@
-import "./ProjectCard.css";
+// ProjectCard.jsx
+
+import React from "react";
+import "../components/ProjectPage.css";
 
 function ProjectCard({ image, title, description, link, linktext }) {
   return (
-    <div className="container">
-      <div className="card-parent">
-        <div className="card-img-parent">
-          <img src={image} alt={title} className="card-img" />
-        </div>
-        <div className="card-body-parent">
-          <div className="card-body-header">{title}</div>
-          <div className="card-body-text">{description}</div>
-        </div>
-        <div className="card-link-parent">
-          <a href={link} target="_blank" rel="noopener noreferrer">
-            {linktext}
-          </a>
-        </div>
+    <div className="card-parent">
+      <div className="card-img-parent">
+        <img src={image} alt={title} className="card-img" />
+      </div>
+      <div className="card-body-parent">
+        <div className="card-body-header">{title}</div>
+        <div className="card-body-text">{description}</div>
+      </div>
+      <div className="card-link-parent">
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          {linktext}
+        </a>
       </div>
     </div>
   );
