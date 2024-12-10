a0549946bb58b394ec0d8eb37d57695f404b295b
BarryByte <abhayraj.12667@gmail.com>
2024-12-10 17:26:36 +0530

    set the default value of sidebar as closed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import { Provider } from "react-redux";
 import "./App.css";
 import store from "./utils/store";
 import Body from "./components/Body";
-import Pattern from "./components/Pattern";
-import Pattern2 from "./components/Pattern2";
+
 
 function App() {
   return (
     <Provider store={store}>
       <div className="relative w-screen h-screen ">
-       
+
         {/* <Pattern2/> */}
         <div className="absolute inset-0 z-10">
           <Body />
